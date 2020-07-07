1edb5089ed81c11ff57b577b0189f68967dbaaf4
gaetanorusso <rs.gaetano@gmail.com>
2020-07-07 15:28:03 +0200

    moved files

---
+++ src/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Private Ledger: Welcome!")
+	/* try this if you want to test */
+	//reset files
+	toClean := []string{"../test/keys.enc"}
+	for _, filename := range toClean {
+		err := os.Remove(filename)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	//init ledger
+	ledger := Ledger{"../test/shards.enc", "../test/keys.enc", "../test/block"}
+	//generate shards and get time-key
+	s := ledger.InitUpdate()
+	//generate user keys
+	u := GenUser()
+	//compute encryption token
+	token := TokenGen(u.PublicKey, s)
+	//add a block
+	index := u.AddBlock(ledger, token, "../test/test.txt")
+	fmt.Println(index)
+	//unlock key from the ledger
+	unlocked := u.UnlockKey(ledger.GetEncKey(index))
+	//decrypt file
+	ledger.DecryptBlock(index, unlocked, "../test/dec.txt")
+	//update ledger
+	sNew := ledger.Update(s)
+	//compare time keys
+	fmt.Println(s.ToString())
+	fmt.Println(sNew.ToString())
+	//get updated encapsulated key from ledger
+	keyEncNew := ledger.GetEncKey(index)
+	//unlock key
+	unlockedNew := u.UnlockKey(keyEncNew)
+	//decrypt file again
+	ledger.DecryptBlock(index, unlockedNew, "../test/dec2.txt")
+}
--- src/protocol.go
+++ src/protocol.go
@@ -4,7 +4,6 @@ import (
 	"crypto/rand"
 	"fmt"
 	"io/ioutil"
-	"os"
 
 	"github.com/gaetanorusso/public_ledger_sensitive_data/miracl/go/core/BN254"
 	"golang.org/x/crypto/sha3"
@@ -35,45 +34,6 @@ var B2 *BN254.ECP2 = BN254.ECP2_generator()
 
 //FP12LEN array len FP12 elements
 const FP12LEN = BN254.MODBYTES*11 + 32
-
-func main() {
-	fmt.Println("Private Ledger: Welcome!")
-	/* try this if you want to test */
-	//reset files
-	toClean := []string{"keys.enc"}
-	for _, filename := range toClean {
-		err := os.Remove(filename)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-	//init ledger
-	ledger := Ledger{"shards.enc", "keys.enc", "block"}
-	//generate shards and get time-key
-	s := ledger.InitUpdate()
-	//generate user keys
-	u := GenUser()
-	//compute encryption token
-	token := TokenGen(u.PublicKey, s)
-	//add a block
-	index := u.AddBlock(ledger, token, "test.txt")
-	fmt.Println(index)
-	//unlock key from the ledger
-	unlocked := u.UnlockKey(ledger.GetEncKey(index))
-	//decrypt file
-	ledger.DecryptBlock(index, unlocked, "dec.txt")
-	//update ledger
-	sNew := ledger.Update(s)
-	//compare time keys
-	fmt.Println(s.ToString())
-	fmt.Println(sNew.ToString())
-	//get updated encapsulated key from ledger
-	keyEncNew := ledger.GetEncKey(index)
-	//unlock key
-	unlockedNew := u.UnlockKey(keyEncNew)
-	//decrypt file again
-	ledger.DecryptBlock(index, unlockedNew, "dec2.txt")
-}
 
 //goodExp check that e is in [2..ORDER -1]
 func goodExp(e *BN254.BIG) bool {
